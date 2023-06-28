98f5a2658953df18a7d91ad0524376bbe23ed52f
bentohset <toh.benjamin123@gmail.com>
2023-06-27 22:23:30 -0400

    fix linting

--- service/routes.py
+++ service/routes.py
@@ -90,7 +90,6 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
     app.logger.info("Request to update an account with id: %s", account_id)
@@ -108,21 +107,19 @@ def update_account(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
     app.logger.info("Request to delete account with id: %s", account_id)
 
     account = Account.find(account_id)
-    if account: account.delete()
+    if account:
+        account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
